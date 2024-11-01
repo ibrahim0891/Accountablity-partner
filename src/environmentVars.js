17432202a9dc3b018bf3bf32ef4c2363b7640f44
ibrahim0891 <nijhum0891@gmail.com>
2024-11-02 00:39:56 +0600

    updated base url of backend service

--- src/environmentVars.js
+++ src/environmentVars.js
@@ -2,5 +2,5 @@
 
 export const env = {
     // BASE_URL: 'https://accountablity-partner-server.onrender.com',
-    BASE_URL: 'https://accountablity-partner-server-production.up.railway.app/',
+    BASE_URL: 'https://accountablity-partner-server-production.up.railway.app',
 }
